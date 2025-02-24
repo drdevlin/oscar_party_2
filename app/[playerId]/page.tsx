366105a72d136d929cf0f88d9af94c1bea0d642f
drdevlin <drdevlin@fastmail.com>
2025-02-24 15:08:49 -0500

    feat: add HideSelections button to selections page

--- app/[playerId]/page.tsx
+++ app/[playerId]/page.tsx
@@ -3,6 +3,7 @@ import { getSession } from '@/lib/jwt';
 import { Back } from '@/components/Back';
 import { Selection } from '@/components/Selection';
 import { Auth } from '@/components/Auth';
+import { HideSelections } from '@/components/HideSelections';
 
 import type { RecordModel } from 'pocketbase';
 import type { Category as CategoryType, Player } from '@/lib/types';
@@ -14,8 +15,17 @@ export interface SelectionsProps {
 }
 export default async function Selections({ params }: SelectionsProps) {
   const { playerId } = await params;
+
   const user = await getSession();
-  const locked = user === null || user !== playerId;
+  const selectionsBelongToUser = user === playerId;
+  const locked = user === null || !selectionsBelongToUser;
+
+  const { hideSelections } = await pb.collection('players').getOne(playerId, {
+    fields: 'hideSelections',
+    cache: 'no-store',
+  }) as Pick<Player, 'hideSelections'>;
+
+  const hidden = hideSelections && !selectionsBelongToUser;
 
   const categories = await pb.collection('categories').getFullList() as CategoryType[];
   const selections = parse(
@@ -33,8 +43,12 @@ export default async function Selections({ params }: SelectionsProps) {
         <Back href="/" />
         <h1>Selections</h1>
         <Auth playerId={playerId} differentPlayer={locked && user !== null} locked={locked} />
+        {selectionsBelongToUser && <HideSelections playerId={playerId} />}
       </div>
-      {categories.map((category) => {
+      {hidden ? (
+        <p>Player has hidden their selections.</p>
+      ) : (
+        categories.map((category) => {
         const selection = selections.find((selection) => selection.categoryId === category.id);
         return (
           <Selection
@@ -47,7 +61,9 @@ export default async function Selections({ params }: SelectionsProps) {
             locked={locked}
           />
         );
-      })}
+      })
+    )
+    }
     </>
   );
 }
@@ -55,7 +71,7 @@ export default async function Selections({ params }: SelectionsProps) {
 export const generateStaticParams = async () => {
   const players = await pb.collection('players').getFullList({
     cache: 'no-store',
-    next: { tags: ['players' ]},
+    next: { tags: ['players'] },
   }) as Player[];
 
   return players.map(({ id }) => ({ playerId: id }));
